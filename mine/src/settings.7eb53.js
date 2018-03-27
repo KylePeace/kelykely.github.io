39fc6895657a22e970162cce676282d417e5376e
kelykely <305258029@qq.com>
2018-03-28 00:20:07 +0800

    创建版本

---
+++ mine/src/settings.7eb53.js
@@ -0,0 +1,659 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "0": [
+                "resources/prefab/lose.prefab",
+                4
+            ],
+            "1": [
+                "resources/plist/lose",
+                2,
+                1
+            ],
+            "3": [
+                "resources/gameUI/flagset",
+                2,
+                1
+            ],
+            "4": [
+                "resources/gameUI/cell1",
+                2,
+                1
+            ],
+            "5": [
+                "resources/plist/win",
+                2,
+                1
+            ],
+            "6": [
+                "resources/plist/win",
+                2,
+                1
+            ],
+            "7": [
+                "resources/plist/win",
+                2,
+                1
+            ],
+            "8": [
+                "resources/amin/win.anim",
+                1
+            ],
+            "9": [
+                "resources/plist/win",
+                2,
+                1
+            ],
+            "10": [
+                "resources/plist/win",
+                2,
+                1
+            ],
+            "11": [
+                "resources/plist/win",
+                2,
+                1
+            ],
+            "12": [
+                "resources/plist/win",
+                2,
+                1
+            ],
+            "13": [
+                "resources/plist/win",
+                2,
+                1
+            ],
+            "14": [
+                "resources/plist/win",
+                2,
+                1
+            ],
+            "15": [
+                "resources/prefab/win.prefab",
+                4
+            ],
+            "16": [
+                "resources/plist/win.plist",
+                3
+            ],
+            "17": [
+                "resources/plist/lose",
+                2,
+                1
+            ],
+            "18": [
+                "resources/plist/lose",
+                2,
+                1
+            ],
+            "19": [
+                "resources/plist/lose",
+                2,
+                1
+            ],
+            "20": [
+                "resources/plist/lose",
+                2,
+                1
+            ],
+            "21": [
+                "resources/plist/lose",
+                2,
+                1
+            ],
+            "22": [
+                "resources/plist/lose",
+                2,
+                1
+            ],
+            "23": [
+                "resources/plist/lose",
+                2,
+                1
+            ],
+            "24": [
+                "resources/amin/lose.anim",
+                1
+            ],
+            "25": [
+                "resources/plist/lose",
+                2,
+                1
+            ],
+            "26": [
+                "resources/plist/lose.plist",
+                3
+            ],
+            "27": [
+                "resources/plist/lose",
+                2,
+                1
+            ],
+            "28": [
+                "resources/plist/lose",
+                2,
+                1
+            ],
+            "29": [
+                "resources/plist/lose",
+                2,
+                1
+            ],
+            "31": [
+                "resources/gameUI/cell_light",
+                2,
+                1
+            ],
+            "32": [
+                "resources/prefab/cell.prefab",
+                4
+            ],
+            "34": [
+                "resources/gameUI/cell0",
+                2,
+                1
+            ],
+            "35": [
+                "resources/plist/boom",
+                2,
+                1
+            ],
+            "36": [
+                "resources/plist/boom",
+                2,
+                1
+            ],
+            "37": [
+                "resources/plist/boom",
+                2,
+                1
+            ],
+            "38": [
+                "resources/plist/boom",
+                2,
+                1
+            ],
+            "39": [
+                "resources/plist/boom",
+                2,
+                1
+            ],
+            "40": [
+                "resources/plist/boom",
+                2,
+                1
+            ],
+            "41": [
+                "resources/plist/boom",
+                2,
+                1
+            ],
+            "42": [
+                "resources/plist/boom",
+                2,
+                1
+            ],
+            "43": [
+                "resources/plist/boom",
+                2,
+                1
+            ],
+            "44": [
+                "resources/plist/boom",
+                2,
+                1
+            ],
+            "45": [
+                "resources/plist/boom",
+                2,
+                1
+            ],
+            "46": [
+                "resources/plist/boom.plist",
+                3
+            ],
+            "47": [
+                "resources/plist/boom",
+                2,
+                1
+            ],
+            "a8Anh32NZGRZegUtSgEj26": [
+                "res/Texture/singleColor.png",
+                0
+            ],
+            "29/FLeXOdNoYj94WGXITRS": [
+                "resources/block",
+                2,
+                1
+            ],
+            "b3wbcI4/NOOpJgMXluFwU8": [
+                "resources/block.png",
+                0
+            ],
+            "67jiGEwzxGOoe4rEGNKM+T": [
+                "resources/gameUI/1",
+                2,
+                1
+            ],
+            "17o8PET9NNSrXDK6T/DcA7": [
+                "resources/gameUI/1.png",
+                0
+            ],
+            "c0CCToWwhBuKzT0ieSTj/a": [
+                "resources/gameUI/2",
+                2,
+                1
+            ],
+            "36akixLVlKpJu9Tpkge9i1": [
+                "resources/gameUI/2.png",
+                0
+            ],
+            "e5bk5iNrJJzqp//jhw9BO8": [
+                "resources/gameUI/3",
+                2,
+                1
+            ],
+            "adzImbuoZCHKI4vQJKKI2l": [
+                "resources/gameUI/3.png",
+                0
+            ],
+            "59MQyNfztGa5RTHkNzs+Fy": [
+                "resources/gameUI/4",
+                2,
+                1
+            ],
+            "92k6HAdfJMI6HzmwTSAFhK": [
+                "resources/gameUI/4.png",
+                0
+            ],
+            "acgqdbR7hJj7INCOmdFeLf": [
+                "resources/gameUI/5",
+                2,
+                1
+            ],
+            "e998aKcipIbq6AmtjM+5MX": [
+                "resources/gameUI/5.png",
+                0
+            ],
+            "5fkh8B1aVIyLnPDy1XDUbM": [
+                "resources/gameUI/6",
+                2,
+                1
+            ],
+            "51smcjMopFJYW6jj5TXcDJ": [
+                "resources/gameUI/6.png",
+                0
+            ],
+            "7a7IiReJpLO47ZjYlCS9tS": [
+                "resources/gameUI/7",
+                2,
+                1
+            ],
+            "a09XwxQ6FFNbDsNLVQNGQl": [
+                "resources/gameUI/7.png",
+                0
+            ],
+            "f4+gweDmhNZKBo0IDBHdWp": [
+                "resources/gameUI/8",
+                2,
+                1
+            ],
+            "29xZ/kyd9A06KwkdFZi7e4": [
+                "resources/gameUI/8.png",
+                0
+            ],
+            "81YPWT5P1JGK5mk0MHRGiE": [
+                "resources/gameUI/cell",
+                2,
+                1
+            ],
+            "b4xnyBFSBGzKccN+ICdbHn": [
+                "resources/gameUI/cell.png",
+                0
+            ],
+            "6b9ankwsxKQZn9CiptU8xU": [
+                "resources/gameUI/cell0.png",
+                0
+            ],
+            "7ewT17PHVCsr6wFj1GU+ei": [
+                "resources/gameUI/cell1.png",
+                0
+            ],
+            "f6k/r/hxhFCpt4iM45LVKb": [
+                "resources/gameUI/cell_light.png",
+                0
+            ],
+            "97vKVmVNJLppEufhDBpBZl": [
+                "resources/gameUI/cm12",
+                2,
+                1
+            ],
+            "ecR/UTVNdIxJ8UxW/w7583": [
+                "resources/gameUI/cm12.png",
+                0
+            ],
+            "162xEQ+bxAmqV2DRsVvSZN": [
+                "resources/gameUI/cm2",
+                2,
+                1
+            ],
+            "d5PnzQiVpAFbIj2boGLCjJ": [
+                "resources/gameUI/cm2.png",
+                0
+            ],
+            "74bUM+tEtCdKtFNBp7YeVs": [
+                "resources/gameUI/flagnone",
+                2,
+                1
+            ],
+            "9fq8BeMLNDA6naccgjrHxc": [
+                "resources/gameUI/flagnone.png",
+                0
+            ],
+            "bcUFKkdS5N9rGCCO1mwXtQ": [
+                "resources/gameUI/flagset.png",
+                0
+            ],
+            "7b4K0xa8JB0Z2lttQ1lqwv": [
+                "resources/gameUI/mine",
+                2,
+                1
+            ],
+            "8dx9lm9YBKgqoF/oGv0ACP": [
+                "resources/gameUI/mine.png",
+                0
+            ],
+            "9a96WdGkZKN4e1ydEfjG5O": [
+                "resources/plist/boom",
+                2,
+                1
+            ],
+            "b8qnswOAxMjKNmqSWvn7o/": [
+                "resources/plist/boom.png",
+                0
+            ],
+            "beaNZBFRdGVpn4KDXED26o": [
+                "resources/plist/lose",
+                2,
+                1
+            ],
+            "03y4s1IZ5LGa+JhLYRdSso": [
+                "resources/plist/lose.png",
+                0
+            ],
+            "e8vGRYteVISbV+G2+ZjFig": [
+                "resources/plist/win",
+                2,
+                1
+            ],
+            "dfGjtF3dhDEZV8t+5fozQ2": [
+                "resources/plist/win.png",
+                0
+            ],
+            "e9b9pDGjVGVIpgpv9t4rfZ": [
+                "resources/spine/building_explode_effect",
+                2,
+                1
+            ],
+            "6eW+macPtCRLkjoxDwITzJ": [
+                "resources/spine/building_explode_effect.atlas",
+                5
+            ],
+            "1dOBheY0xIUriKs5rvtBYY": [
+                "resources/spine/building_explode_effect.json",
+                6
+            ],
+            "00Oq7KpN9PFqX4S3aQpHtX": [
+                "resources/spine/building_explode_effect.png",
+                0
+            ]
+        },
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                0
+            ],
+            "e8Ueib+qJEhL6mXAHdnwbi": [
+                "image/default_btn_normal.png",
+                0
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                0
+            ],
+            "d8HsitJHxOYqo801xBk8ev": [
+                "image/default_panel.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D",
+        "cc.AnimationClip",
+        "cc.SpriteFrame",
+        "cc.SpriteAtlas",
+        "cc.Prefab",
+        "cc.RawAsset",
+        "sp.SkeletonData"
+    ],
+    launchScene: "db://assets/scenes/game.fire",
+    scenes: [
+        {
+            url: "db://assets/scenes/game.fire",
+            uuid: 2
+        }
+    ],
+    packedAssets: {
+        "01ea6936c": [
+            0,
+            1
+        ],
+        "01ee63c52": [
+            30,
+            2,
+            "41D7kWhyFGY7q4NDlzkazn",
+            3,
+            4,
+            "9bvaMerUlDyary99mJa6xp",
+            33,
+            "f0BIwQ8D5Ml7nTNQbh1YlS"
+        ],
+        "0314e9fdc": [
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14
+        ],
+        "03f8f79ce": [
+            15,
+            11
+        ],
+        "04bfb3b9": [
+            5,
+            6,
+            7,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            16
+        ],
+        "060166b9a": [
+            17,
+            18,
+            19,
+            20,
+            21,
+            22,
+            23,
+            24,
+            1,
+            25
+        ],
+        "0ad81166e": [
+            26,
+            27,
+            17,
+            18,
+            19,
+            28,
+            20,
+            21,
+            29,
+            22,
+            23,
+            1,
+            25
+        ],
+        "0ad96203d": [
+            30,
+            3,
+            31,
+            32,
+            33,
+            34
+        ],
+        "0b4dcb51": [
+            35,
+            36,
+            37,
+            38,
+            39,
+            40,
+            41,
+            42,
+            43,
+            44,
+            45,
+            46,
+            47
+        ]
+    },
+    orientation: "portrait",
+    uuids: [
+        "58wzFIiGJHc7p5+sYdLGF3",
+        "e5UHnjU6ZGW7Tz+YWxKjBh",
+        "2dL3kvpAxJu6GJ7RdqJG5J",
+        "56GFcg4zVEwK5aXrDSjAI2",
+        "5dTXStqr1B85AwIheBkNAD",
+        "02jrB8ePRPdach0l1fWoJC",
+        "0dXkv88/BPy5xALADq3K3/",
+        "14u3ZWBOJJBpg9FtxrcLFz",
+        "4a/R+DA2ROgpVBL6CUki+m",
+        "76TuLuwdJKE7PVAV3Du+2e",
+        "bayWRYfUFKbZ1qzPiPk96w",
+        "c6zyYeCeZE9KrBIJYlUVxd",
+        "e9Zc4UA6RKnKcEMuDMpaLb",
+        "ecs+hj+f5FkpQc+DWEH/ph",
+        "f1omnGU+VGB6U4pWP1J6Mw",
+        "bdsIMWqLVCyqaQsOsJ9cgK",
+        "fb7RBnUP1O3rQZaSAEyIU6",
+        "3aSbkUvzFPVZdTxki531Fj",
+        "69heMCeT1PHaWYBRyKX7j0",
+        "7aF18wngVPELiJMqdFkcR0",
+        "9cGJsY5ohG3IE7dMb/32OV",
+        "a6V9MeIm1CMKoGf9JHT9s3",
+        "b08MqkVmdOvqcEPhtqlDEe",
+        "b5aUl21mxCTZnTIY36o/cF",
+        "c0p13yqAlATr9t7PDLSpuF",
+        "eaY13weRdPu5YbmKc0s+RL",
+        "03je0kZKdP1qZ/C5D6S2A1",
+        "2aw4Lea5JECqzW6nDfbJWG",
+        "7b162lt7BJxKq9cmC//17J",
+        "afK59Dh01HxJXo0lHy5xsg",
+        "29FYIk+N1GYaeWH/q1NxQO",
+        "bcvlfXzXVNIZbSI+7ayiBj",
+        "d6zjoJeuZD/7iDDUoRQuhq",
+        "e97GVMl6JHh5Ml5qEDdSGa",
+        "f31RS3Gr9O4K5M5q+HcDTR",
+        "0cCS1vQ2hK/pQpciuZaMVI",
+        "12H+43Ie9NS6GuFXV5aVQ6",
+        "1bVE+JQdFBL5F3zFdHMaK/",
+        "4aNzxCPT5DlLojTnc6Rhe4",
+        "6doWoVLHNDYoA3m6oQxFLo",
+        "75sJmESQxLSIcNsU685J3n",
+        "797AC02Q9IkIDDQx0WC+c7",
+        "9eJMCO2cpOrL60J0o0zcHQ",
+        "b1xn7nlCBPwLkXIc/whCcH",
+        "bcxtj3NW1JA75aiTFb8B9Z",
+        "f2putosrhBQ6gX1Cqlrn79",
+        "f3ENF/EwFEXaxwjsO9XcpN",
+        "f6txEj6PFF2JOyy+Y1c8kY"
+    ],
+    "md5AssetsMap": {
+        "01/01ea6936c.json": "d04ac",
+        "01/01ee63c52.json": "9561d",
+        "03/0314e9fdc.json": "26b20",
+        "03/03f8f79ce.json": "f5bd3",
+        "04/04bfb3b9.json": "ba3e9",
+        "06/060166b9a.json": "4dace",
+        "0a/0ad81166e.json": "c6039",
+        "0a/0ad96203d.json": "0e756",
+        "0b/0b4dcb51.json": "64f83",
+        "16/16db1110-f9bc-409a-a576-0d1b15bd264d.json": "48c3e",
+        "1d/1d38185e-634c-4852-b88a-b39aefb41618.json": "52bd7",
+        "29/29fc52de-5ce7-4da1-88fd-e16197213452.json": "2c058",
+        "59/59310c8d-7f3b-466b-9453-1e4373b3e172.json": "ebc03",
+        "5f/5f921f01-d5a5-48c8-b9cf-0f2d570d46cc.json": "42254",
+        "67/678e2184-c33c-463a-87b8-ac418d28cf93.json": "48efd",
+        "74/746d433e-b44b-4274-ab45-341a7b61e56c.json": "80073",
+        "7a/7aec8891-789a-4b3b-8ed9-8d89424bdb52.json": "ebb6c",
+        "7b/7be0ad31-6bc2-41d1-9da5-b6d43596ac2f.json": "5fa3e",
+        "81/8160f593-e4fd-4918-ae66-934307446884.json": "8d8eb",
+        "97/97bca566-54d2-4ba6-912e-7e10c1a41665.json": "a0608",
+        "9a/9af7a59d-1a46-4a37-87b5-c9d11f8c6e4e.json": "87f65",
+        "ac/ac82a75b-47b8-498f-b20d-08e99d15e2df.json": "33ede",
+        "be/be68d641-1517-4656-99f8-2835c40f6ea8.json": "098fc",
+        "c0/c00824e8-5b08-41b8-acd3-d227924e3fda.json": "e8707",
+        "e5/e56e4e62-36b2-49ce-aa7f-fe3870f413bc.json": "58406",
+        "e8/e8bc6458-b5e5-4849-b57e-1b6f998c58a0.json": "182b4",
+        "e9/e96fda43-1a35-4654-8a60-a6ff6de2b7d9.json": "8e5b4",
+        "f4/f4fa0c1e-0e68-4d64-a068-d080c11dd5a9.json": "31ba4",
+        "assets/res/Texture/singleColor.png": "e5136",
+        "assets/resources/block.png": "252e9",
+        "assets/resources/gameUI/1.png": "939b5",
+        "assets/resources/gameUI/2.png": "4c062",
+        "assets/resources/gameUI/3.png": "cbdf2",
+        "assets/resources/gameUI/4.png": "0bb29",
+        "assets/resources/gameUI/5.png": "3e50f",
+        "assets/resources/gameUI/6.png": "68d89",
+        "assets/resources/gameUI/7.png": "4a459",
+        "assets/resources/gameUI/8.png": "5635b",
+        "assets/resources/gameUI/cell_light.png": "2b59d",
+        "assets/resources/gameUI/cell.png": "2b59d",
+        "assets/resources/gameUI/cell0.png": "3a67a",
+        "assets/resources/gameUI/cell1.png": "455a3",
+        "assets/resources/gameUI/cm12.png": "b7a28",
+        "assets/resources/gameUI/cm2.png": "8e1bb",
+        "assets/resources/gameUI/flagnone.png": "fb9a5",
+        "assets/resources/gameUI/flagset.png": "abbf4",
+        "assets/resources/gameUI/mine.png": "617c2",
+        "assets/resources/plist/boom.png": "7c517",
+        "assets/resources/plist/lose.png": "5b351",
+        "assets/resources/plist/win.png": "2168f",
+        "assets/resources/spine/building_explode_effect.atlas": "d1cca",
+        "assets/resources/spine/building_explode_effect.png": "f3dd7",
+        "internal/image/default_btn_disabled.png": "286c6",
+        "internal/image/default_btn_normal.png": "1ecb7",
+        "internal/image/default_btn_pressed.png": "bedf4",
+        "internal/image/default_panel.png": "cdbc9"
+    }
+};
